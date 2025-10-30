9cce54218018cce754b0f164f468a2a9cc9f4eec
Syed Hassan Raza <hassanraza375@gmail.com>
2025-10-30 12:43:07 -0700

    homepage

--- js/header.js
+++ js/header.js
@@ -1,14 +1,25 @@
 const header = `
-<a href="index.html"><img src="assets/index/header-sehatti-posts.png" class="header-sehatti-posts" /></a>
-		
-		<div class="header-nav">
-			<p class="header-text"><a href="about-us.html">About Us</a></p>
-            <p class="header-text"><a href="ecosystem.html">Ecosystem</a></p>
-            <p class="header-text"><a href="solutions.html">Solutions</a></p>
-            <p class="header-text"><a href="resources.html">Resources</a></p>
-            <p class="header-text"><a href="ai-insights.html">AI Insights</a></p>
-		</div>
-		
-		<a href="request-a-demo.html" class="btn-cta header-btn-cta hover-dark">Request A Demo</a>`;
+    <a href="index.html">
+        <img src="assets/index/header-sehatti-posts.png" class="header-sehatti-posts">
+    </a>
 
-document.querySelector('#header').innerHTML = header;
+    <!-- BURGER MENU BUTTON (hidden on desktop) -->
+    <div class="burger" id="burger">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+
+    <!-- NAVIGATION -->
+    <div class="header-nav" id="mobileNav">
+        <p class="header-text"><a href="about-us.html">About Us</a></p>
+        <p class="header-text"><a href="ecosystem.html">Ecosystem</a></p>
+        <p class="header-text"><a href="solutions.html">Solutions</a></p>
+        <p class="header-text"><a href="resources.html">Resources</a></p>
+        <p class="header-text"><a href="ai-insights.html">AI Insights</a></p>
+    </div>
+
+    <a href="request-a-demo.html" class="btn-cta header-btn-cta hover-dark">Request A Demo</a>
+`;
+
+document.querySelector("#header").innerHTML = header;
