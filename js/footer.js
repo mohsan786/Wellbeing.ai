94900e5655eef3484cdc126f920b9c149d1bef55
mohsan786 <162111522+mohsan786@users.noreply.github.com>
2025-10-30 18:59:01 +0500

    initial commit

---
+++ js/footer.js
@@ -0,0 +1,61 @@
+const footer =`
+<div class="footer-col-left">
+			<a href="/index.html"><img src="assets/index/footer-sehatti-posts.png" class="footer-sehatti-posts" /></a>
+			<p class="text-b footer-text-the-gcc-s-first">
+				The GCC's first AI-powered wellbeing and mental health ecosystem
+			</p>
+			<object data="assets/footer-social-media.svg" class="footer-social-media" type="image/svg+xml"></object>
+		</div>
+		
+		<div class="footer-col-right">
+			<div class="footer-row">
+				<div class="column-c col3">
+					<p class="column-text3">Product</p>
+					
+					<div class="column-col">
+						<p class="text-b">HR Portal</p>
+						<p class="text-b">Employee Portal</p>
+						<p class="text-b">AI Consultant</p>
+						<p class="text-b">Admin Portal</p>
+					</div>
+				</div>
+				
+				<div class="column-c col4">
+					<p class="column-text3">Solutions</p>
+					
+					<div class="column-col">
+						<p class="text-b">For Corporates</p>
+						<p class="text-b">For Government</p>
+						<p class="text-b">For Education</p>
+						<p class="text-b">For Healthcare</p>
+					</div>
+				</div>
+				
+				<div class="column-c col5">
+					<p class="column-text3">Resources</p>
+					
+					<div class="column-col">
+						<p class="text-b">Blogs</p>
+						<p class="text-b">Case Studies</p>
+						<p class="text-b">Webinars</p>
+						<p class="text-b">Documentations</p>
+					</div>
+				</div>
+				
+				<div class="column-c col6">
+					<p class="column-text3">Company</p>
+					
+					<div class="column-col">
+						<p class="text-b">About Us</p>
+						<p class="text-b">Careers</p>
+						<p class="text-b">Contact</p>
+						<p class="text-b">Privacy Policy</p>
+					</div>
+				</div>
+			</div>
+			
+			<p class="footer-text-copyright-2025-sehatti">© 2025 Sehatti. All rights reserved.</p>
+		</div>
+`;
+
+document.querySelector('#footer').innerHTML = footer;
